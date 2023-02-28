2d5f174c990811facdc608725465700a14f6a39c
BrunoFNRodrigues <bf17669@gmail.com>
2023-02-28 20:22:54 -0300

    ver 1.1.0

--- main.py
+++ main.py
@@ -14,51 +14,78 @@ class Tokenizer():
 
     def selectNext(self):
         l = self.source.replace(" ", "")
-        num = re.split("[+-]", l)
+        num = re.split("[+-/*]", l)
         sin = re.split("[0123456789]", l)
         sin = [x for x in sin if x != '']
+        if (self.position >= len(num)+len(sin)):
+            self.next = Token("EOF", 0) 
+            return
+        
         if (self.position)%2:
-            if sin[int(self.position/2//1)] == "-":
+            sinal = sin[int(self.position/2//1)]
+            if sinal == "-":
                 self.next = Token("NEG", -1)
-            else:
+            elif sinal == "+":
                 self.next = Token("POS", 1)
+            elif sinal == "/":
+                self.next = Token("DIV", 1)
+            elif sinal == "*":
+                self.next = Token("MULT", 1)
+     
         else:
             self.next = Token("INT",int(num[int(self.position/2)]))
         self.position += 1
-        if (self.position >= len(num)+len(sin)):
-            self.position = -1 
-            return
+
 
 
 class Parser():
     tokenizer = None
 
-    def parseExpression():
-        # Da erro sintatico
-        a = Parser.tokenizer.next.value
-
-        while Parser.tokenizer.position > 0:
-            Parser.tokenizer.selectNext()
-            sinal = Parser.tokenizer.next.value
-            Parser.tokenizer.selectNext()
-            b =  Parser.tokenizer.next.value
-            a = a + b*sinal
-        return a
-
     def parseTerm():
-        while 1:
-            res = Parser.parseExpression()
-        return 0
+        if Parser.tokenizer.next.type == "INT":
+            res = Parser.tokenizer.next.value
+            Parser.tokenizer.selectNext()
+            while Parser.tokenizer.next.type == "MULT" or Parser.tokenizer.next.type == "DIV":
+                if Parser.tokenizer.next.type == "MULT":
+                    Parser.tokenizer.selectNext()
+                    if Parser.tokenizer.next.type == "INT":
+                        res *= Parser.tokenizer.next.value
+                    else:
+                        raise Exception("Erro sintático: sinais consecutivos")    
+                elif Parser.tokenizer.next.type == "DIV":
+                    Parser.tokenizer.selectNext()
+                    if Parser.tokenizer.next.type == "INT":
+                        res /= Parser.tokenizer.next.value
+                    else:
+                        raise Exception("Erro sintático: sinais consecutivos")  
+                Parser.tokenizer.selectNext()
+            return res
+        else:
+            raise Exception("Valor inicial inválido")
+                    
+
+    def parseExpression():
+        res = Parser.parseTerm()
+        while Parser.tokenizer.next.type == "POS" or Parser.tokenizer.next.type == "NEG":
+            if Parser.tokenizer.next.type == "POS":
+                Parser.tokenizer.selectNext()
+                res += Parser.parseTerm()
+                  
+            elif Parser.tokenizer.next.type == "NEG":
+                Parser.tokenizer.selectNext()
+                res -= Parser.parseTerm()
+
+        return int(res)
 
     def run(code):
         lexico(code)
         sintatico(code)
-        Parser.tokenizer = Tokenizer(code,1,Token("INT", int(re.split("[+-]", code.replace(" ", ""))[0])))
+        Parser.tokenizer = Tokenizer(code,1,Token("INT", int(re.split("[+-/*]", code.replace(" ", ""))[0])))
         return Parser.parseExpression()
 
 
 def lexico(arg):
-    alfabeto = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", " "]
+    alfabeto = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "*", "/", " "]
     for i in arg:
         if i not in alfabeto:
             raise Exception("Invalid Argument")

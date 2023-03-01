1e36aa2bd44795644d9c4111a0b3b72c7022dd81
BrunoFNRodrigues <bf17669@gmail.com>
2023-02-28 23:40:55 -0300

    ver 1.1.1

--- main.py
+++ main.py
@@ -12,29 +12,76 @@ class Tokenizer():
         self.position = position
         self.next = next
 
-    def selectNext(self):
-        l = self.source.replace(" ", "")
-        num = re.split("[+-/*]", l)
-        sin = re.split("[0123456789]", l)
-        sin = [x for x in sin if x != '']
-        if (self.position >= len(num)+len(sin)):
-            self.next = Token("EOF", 0) 
-            return
+    # def selectNext(self):
+    #     l = self.source.replace(" ", "")
+    #     num = re.split("[+-/*]", l)
+    #     sin = re.split("[0123456789]", l)
+    #     sin = [x for x in sin if x != '']
+    #     if (self.position >= len(num)+len(sin)):
+    #         self.next = Token("EOF", 0) 
+    #         return
         
-        if (self.position)%2:
-            sinal = sin[int(self.position/2//1)]
-            if sinal == "-":
-                self.next = Token("NEG", -1)
-            elif sinal == "+":
-                self.next = Token("POS", 1)
-            elif sinal == "/":
-                self.next = Token("DIV", 1)
-            elif sinal == "*":
-                self.next = Token("MULT", 1)
+    #     if (self.position)%2:
+    #         sinal = sin[int(self.position/2//1)]
+    #         if sinal == "-":
+    #             self.next = Token("NEG", -1)
+    #         elif sinal == "+":
+    #             self.next = Token("POS", 1)
+    #         elif sinal == "/":
+    #             self.next = Token("DIV", 1)
+    #         elif sinal == "*":
+    #             self.next = Token("MULT", 1)
      
+    #     else:
+    #         self.next = Token("INT",int(num[int(self.position/2)]))
+    #     self.position += 1
+    def selectNext(self):
+
+        nums = ["0","1","2","3","4","5","6","7","8","9"]
+        sym = ["+","-","/","*"]
+        start_positon = self.position
+        if start_positon == len(self.source):
+            self.next = Token("EOF", 0)
+            
+        elif self.source[self.position] in nums:
+            PARSING = 1
+            while PARSING:
+                if self.source[self.position] in sym:
+                    value = self.source[start_positon:self.position]
+                    if " " in value.strip():
+                        raise Exception("Space between numbers")
+                    else:
+                        self.next = Token("INT", int(value.replace(" ", "")))
+                        PARSING = 0
+                else:
+                    self.position += 1   
+                    if self.position == len(self.source):
+                        self.next = Token("INT", int(self.source[start_positon:self.position].replace(" ", "")))
+                        PARSING = 0
+                
+        elif self.source[self.position] in sym:
+            PARSING = 1
+            while PARSING:
+                if self.source[self.position] in nums:
+                    sinal = self.source[start_positon:self.position].replace(" ", "")
+                    if len(sinal) != 1:
+                        raise Exception("Too many symbols")
+                    else:
+                        if sinal == "-":
+                            self.next = Token("NEG", -1)
+                        elif sinal == "+":
+                            self.next = Token("POS", 1)
+                        elif sinal == "/":
+                            self.next = Token("DIV", 1)
+                        elif sinal == "*":
+                            self.next = Token("MULT", 1)
+                        PARSING = 0
+                else:
+                    self.position += 1
         else:
-            self.next = Token("INT",int(num[int(self.position/2)]))
-        self.position += 1
+            self.position += 1
+            
+
 
 
 
@@ -65,6 +112,7 @@ class Parser():
                     
 
     def parseExpression():
+        Parser.tokenizer.selectNext()
         res = Parser.parseTerm()
         while Parser.tokenizer.next.type == "POS" or Parser.tokenizer.next.type == "NEG":
             if Parser.tokenizer.next.type == "POS":
@@ -79,8 +127,7 @@ class Parser():
 
     def run(code):
         lexico(code)
-        sintatico(code)
-        Parser.tokenizer = Tokenizer(code,1,Token("INT", int(re.split("[+-/*]", code.replace(" ", ""))[0])))
+        Parser.tokenizer = Tokenizer(code,0,Token("INT", 0))
         return Parser.parseExpression()
 
 
@@ -97,7 +144,7 @@ def sintatico(arg):
     if arg.strip()[-1] == "+" or arg.strip()[-1] == "-":
         raise Exception("Invalid end character")
 
-    l = re.split("[+-]", arg)
+    l = re.split("[+-/*]", arg)
     for i in l:
         if " " in i.strip():
             raise Exception("Space between numbers")

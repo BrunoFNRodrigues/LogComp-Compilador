ece5c1561597463b1e7ac942f946c5a92b4f4684
Bruno Rodrigues <bf17669@gmail.com>
2023-03-13 17:29:34 -0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import re
+from abc import ABC, abstractmethod
 
 class Token():
     def __init__(self, type, value):
@@ -175,6 +175,38 @@ class Parser():
             raise Exception("Faltou parenteses")
         return res
 
+class Node(ABC):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+
+    @abstractmethod
+    def Evaluate():
+        None
+
+class BinOp(Node):
+    def Evaluate(self):
+        if self.value == "-":
+            return self.children[1].Evaluate()-self.children[0].Evaluate()
+        elif self.value == "+":
+            return self.children[1].Evaluate()+self.children[0].Evaluate()
+        elif self.value == "*":
+            return self.children[1].Evaluate()*self.children[0].Evaluate()
+        return self.children[1].Evaluate()/self.children[0].Evaluate()
+
+class UnOp(Node):
+    def Evaluate(self):
+        if self.value == "-":
+            return -self.children[0].Evaluate()
+        return self.children[0].Evaluate()
+        
+class IntVal(Node):
+    def Evaluate(self):
+        return self.value
+    
+class NoOp(Node):
+    def Evaluate():
+        return None
 
 def lexicon(arg):
     alfabeto = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "*", "/", " ", "(", ")"]

776c58d285df1c9856255e96ecfe84abe8348897
BrunoFNRodrigues <bf17669@gmail.com>
2023-05-21 20:50:37 -0300

    nova ast

--- AST.py
+++ AST.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from SymbolTable import SymbolTable
+from FuncTable import FuncTable
 
 class Node(ABC):
     def __init__(self, value=0, children=[]):
@@ -7,11 +8,11 @@ class Node(ABC):
         self.children = children
 
     @abstractmethod
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         None
 
 class BinOp(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         if self.value == "==":
             res = self.children[1].Evaluate()[1] == self.children[0].Evaluate()[1]
         elif self.value == ">":
@@ -36,7 +37,7 @@ class BinOp(Node):
         return ("Int", int(res))
 
 class UnOp(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         if self.children.Evaluate()[0] != "Int":
             raise Exception("UnOP não aceita"+self.children.Evaluate()[0])
         if self.value == "-":
@@ -47,61 +48,87 @@ class UnOp(Node):
             return ("Int", not self.children.Evaluate()[1])
 
 class ConcatOp(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         return ("String" ,str(self.children[1].Evaluate()[1]) + str(self.children[0].Evaluate()[1]))
 
 class IntVal(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         return ("Int", self.value)
 
 class StringVal(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         return ("String", self.value)
 
 class NoOp(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         return None
 
 class Identifier(Node):
-    def Evaluate(self):
-        return SymbolTable.Getter(self.value)
+    def Evaluate(self, table = []):
+        return table.Getter(self.value)
     
 class Block(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         for child in self.children:
-            child.Evaluate()
+            child.Evaluate(table)
+            if isinstance(child, Return):
+                break
 
 class Print(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
             print(self.children.Evaluate()[1])
 
 class Assignment(Node):
-    def Evaluate(self):
-        SymbolTable.Setter(self.children[0].value, self.children[1].Evaluate())
+    def Evaluate(self, table = []):
+        table.Setter(self.children[0].value, self.children[1].Evaluate())
     
 class Read(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         return ("Int", int(input()))
     
 class While(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         while self.children[1].Evaluate()[1]:
             self.children[0].Evaluate()
 
 class If(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         if self.children[-1].Evaluate():
             self.children[-2].Evaluate()
         elif len(self.children) > 2:
             self.children[0].Evaluate()
 
 class VarDec(Node):
-    def Evaluate(self):
+    def Evaluate(self, table = []):
         if len(self.children) <= 1:
             if self.value == "Int":
-                SymbolTable.Create(self.children[0].value,["Int", 0])
+                table.Create(self.children[0].value,["Int", 0])
             elif self.value == "String":
-                SymbolTable.Create(self.children[0].value, ["String", ""])
+                table.Create(self.children[0].value, ["String", ""])
         else:
-            SymbolTable.Create(self.children[0].value, self.children[1].Evaluate())
+            table.Create(self.children[0].value, self.children[1].Evaluate())
+        
+class Return(Node):
+    def Evaluate(self, table = []):
+        return self.children[0].Evaluate()
+
+class FuncDec(Node):
+    def Evaluate(self, table = []):
+        if len(self.children) != len(self.children[1]) + 2:
+            raise Exception("func declarada errada")
+        FuncTable.Setter(self.children[0].value, (self.value, self))
+
+class FuncCall(Node):
+    def Evaluate(self, table = []):
+        func = FuncTable.Getter(self.value)
+        if len(func[1].children[1]) != len(self.children):
+            raise Exception("São "+str(len(func[1].children[1]))+" mas foram recebidos"+ str(len(self.children)))
+        new_st = SymbolTable()
+        for var in func[1].children[1]:
+            var.Evaluate(new_st)    
+        i = 0
+        for key in new_st.table.keys():
+            new_st.Setter(key, self.children[i].Evaluate())
+            i+=1      
+        self.children[3].Evaluate(new_st)   
         
